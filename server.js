f922f6dc742520a79bb2424d850284bbe19fa681
MoldingTofu <jeremykchang@gmail.com>
2019-01-03 01:57:45 -0600

    sqlite3 fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//var sqlite = require('sqlite3');
+var sqlite = require('sqlite3');
 var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
 
-//var db = require('./database.js');
+var db = require('./database.js');
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
