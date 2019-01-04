33afe9fc204a173b066390cba6517941f5c47b83
moldingtofu <jeremykchang@gmail.com>
2019-01-03 22:27:22 -0600

    frontend

--- public/index.js
+++ public/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 
+  $('#create').click(createShort);
+
 });
+
+function createShort() {
+  var url = $('#url').val();
+
+  if (url === '') {
+
+  }
+  else {
+    $.ajax({
+      "method": "POST",
+      "crossDomain": true,
+      "url": "http://localhost:8080/",
+      "data": {
+        "url": url
+      },
+      "success": console.log('success'),
+      "error": function (err) {
+        console.log(error);
+      }
+    });
+  }
+}
